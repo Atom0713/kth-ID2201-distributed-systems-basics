e33f9f9c2517cd49a484978da18a4f5b54fb596c
Artem Sliusarenko <artemsliusarenko@artems-mbp.home>
2023-09-03 10:41:12 +0200

    make server serve requests until it is stopped manually

--- ID2201/HW1_Rudy/rudy.erl
+++ ID2201/HW1_Rudy/rudy.erl
@@ -1,5 +1,11 @@
 -module(rudy).
--export([init/1]).
+-export([init/1, start/1, stop/0]).
+
+start(Port) ->
+    register(rudy, spawn(fun() -> init(Port) end)).
+
+stop() ->
+    exit(whereis(rudy), "time to die").
 
 init(Port) ->
     Opt = [list, {active, false}, {reuseaddr, true}],
@@ -15,7 +21,8 @@ init(Port) ->
 handler(Listen) -> 
     case gen_tcp:accept(Listen) of
         {ok, Client} ->
-            request(Client);
+            request(Client),
+            handler(Listen);
         {error, Error} -> 
             error
     end.
