aaeeafde0f1d4488b9fcdc4a35f98750f931c0d9
Artem Sliusarenko <artemsliusarenko@artems-mbp.home>
2023-09-23 14:58:38 +0200

    update test script, add report

--- HW2_Routy/test.erl
+++ HW2_Routy/test.erl
@@ -7,11 +7,12 @@ run() ->
     routy:start(r3, uppsala),
     routy:start(r4, gothenburg),
     routy:start(r5, malmo),
-    r1 ! {add, lund, {r2, 'sweden@192.168.0.19'}},
-    r2 ! {add, uppsala, {r3, 'sweden@192.168.0.19'}},
-    r3 ! {add, gothenburg, {r4, 'sweden@192.168.0.19'}},
-    r4 ! {add, malmo, {r5, 'sweden@192.168.0.19'}},
-    r5 ! {add, stockholm, {r1, 'sweden@192.168.0.19'}},
+    r1 ! {add, lund, {r2, 'sweden@10.20.139.39'}},
+    r2 ! {add, uppsala, {r3, 'sweden@10.20.139.39'}},
+    r3 ! {add, gothenburg, {r4, 'sweden@10.20.139.39'}},
+    r3 ! {add, malmo, {r5, 'sweden@10.20.139.39'}},
+    r4 ! {add, malmo, {r5, 'sweden@10.20.139.39'}},
+    r5 ! {add, stockholm, {r1, 'sweden@10.20.139.39'}},
 
     timer:sleep(timer:seconds(1)),
     r1 ! broadcast,
@@ -29,30 +30,20 @@ run() ->
 
     %io:get_line("Next?\n"),
     io:format("Ask lund for status from stockholm~n"),
-    r1 ! {status, {r2, 'sweden@192.168.0.19'}},
-    io:format("\n"),
+    r1 ! {status, {r2, 'sweden@10.20.139.39'}},
 
     timer:sleep(timer:seconds(1)),
     io:format("Ask stockholm for status from uppsala~n"),
-    r3 ! {status, {r1, 'sweden@192.168.0.19'}},
-    io:format("\n"),
+    r3 ! {status, {r1, 'sweden@10.20.139.39'}},
 
     timer:sleep(timer:seconds(1)),
     io:format("Send message from stockholm to lund~n"),
     r1 ! {route, lund, stockholm, "Hello"},
-    io:format("\n"),
 
     timer:sleep(timer:seconds(1)),
     io:format("Send message from uppsala to stockholm~n"),
     r3 ! {route, stockholm, uppsala, "Hello"},
-    io:format("\n"),
-
-    timer:sleep(timer:seconds(1)),
-    io:format("Send message from uppsala to lund~n"),
-    r3 ! {route, lund, uppsala, "Hello"},
-    io:format("\n"),
 
     timer:sleep(timer:seconds(1)),
     io:format("Send message from gothenburg to stockholm~n"),
-    r4 ! {route, stockholm, gothenburg, "Hello"},
-    io:format("\n").
+    r4 ! {route, stockholm, gothenburg, "Hello"}.
